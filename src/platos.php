d81628bcdad146bfbb3b27e8c22bb87ac55e0e9d
Dalembert Bravo <dalembertbravo2@gmail.com>
2024-03-31 11:43:21 -0500

    Implementacion del composer en el proyecto para poder trabajar por medio de clases

---
+++ src/platos.php
@@ -0,0 +1,6 @@
+<?php
+namespace  manin;
+
+class Platos{
+  
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'manin/restaurante',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8d4d564c78b8b1a24c64e9b8b4ba2f65cda8a07d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'manin/restaurante' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8d4d564c78b8b1a24c64e9b8b4ba2f65cda8a07d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
