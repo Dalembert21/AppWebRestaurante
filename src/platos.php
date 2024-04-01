b7f17978747754353f7e9d95d51fb348b8989b38
Dalembert Bravo <dalembertbravo2@gmail.com>
2024-03-31 20:24:32 -0500

    creacion de la conexion para la bd mediante pdo

--- src/platos.php
+++ src/platos.php
@@ -2,5 +2,18 @@
 namespace  manin;
 
 class Platos{
-  
+
+  private $configuracion;
+  private $cn = null;
+
+  public function __construct(){
+    //referencia al archivo config. ini
+    $this->configuracion = parse_ini_file(__DIR__.'/../config.ini');
+    print_r($this->configuracion);
+
+    $this->cn = new \PDO($this->configuracion['conectar'],$this->configuracion['usuario'],$this->configuracion['clave']);
+  }
+
+
 }
+
