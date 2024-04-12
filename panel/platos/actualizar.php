a0dc70d4a6df2816c3b3f4e40022cfee25a2ca1a
Dalembert Bravo <dalembertbravo2@gmail.com>
2024-04-11 22:29:50 -0500

    selecciona el combo de forma automatica cuando se edita

--- panel/platos/actualizar.php
+++ panel/platos/actualizar.php
@@ -104,7 +104,20 @@ if(!$resultado)
     <label >Categorias</label>
         <select  class="form-control"name="categoria_id" id="" required>
           <option value="">--Seleccione--</option>
-          <option value="1">Comida de la Region Costa</option>
+                   <?php
+               require '../../vendor/autoload.php';
+                  $categoria = new manin\Categorias;
+                  $info_categoria = $categoria->ver(); 
+                  $cantidad = count($info_categoria);
+                  for($x =0;$x < $cantidad; $x++){
+                    $item = $info_categoria[$x];
+                         ?>
+                           <option value=""<?php print $item['ID_CAT']?>"
+                           <?php print $resultado['CAT_ID_PER'] == $item['ID_CAT'] ?'selected': ''  ?>
+                           ><?php print $item['NOM_CAT']?></option>
+                              <?php
+                  }
+                      ?>option>
         </select>
   </div>
         </div>
