b318ff739189cf4a53e247ff6f87bb314348441c
Dalembert Bravo <dalembertbravo2@gmail.com>
2024-04-22 21:18:53 -0500

    le di funcionamiento al boton eliminar

--- eliminarCarrito.php
+++ eliminarCarrito.php
@@ -1,5 +1,18 @@
 <?php
     session_start();
-    session_destroy();
+    //aseguramos que el valor que viene por la url sea numerico
+    if(isset($_GET['id']) or ! is_numeric($_GET['id']))
+    header('Location: carrito.php');
+     $id = $_GET['id'];
+    //aseguro que la sesion exista
+    if(isset($_SESSION['carrito'])){
+        //remuevo el arreglo dentro de la session
+        unset($_SESSION['carrito'][$id]);
 
+        header('Location: carrito.php');
+    }else{
+        header('Location: index.php');
+      
+    }
+   
 ?>
