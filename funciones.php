9a497d2bb257b839c4bab289dcbaa51c873ca92e
Dalembert Bravo <dalembertbravo2@gmail.com>
2024-04-22 20:58:44 -0500

    cree los metodos para calcular el total y mostrar la cantidad de platos

--- funciones.php
+++ funciones.php
@@ -30,13 +30,29 @@ function agregarPlato($resultado, $id, $cantidad = 1) {
 
 
 
-
+//acumulo lo que tengo el precio por la cantidad
 function calcularTotal(){
+      $total = 0;
+      if(isset($_SESSION['carrito'])){
+            foreach($_SESSION['carrito'] as $indice=>$value){
+                    $total += $value['precio'] * $value['cantidad'];
+                  
+            }
+      }
 
+      return $total;
 
 }
 function cantidadPlatos(){
+      $cantidad = 0;
+      if(isset($_SESSION['carrito'])){
+            foreach($_SESSION['carrito'] as $indice=>$value){
+                    $cantidad++;
+                  
+            }
+      }
 
+      return $cantidad;
 
 }
 ?>
